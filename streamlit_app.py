c1f3f99f1df99ed9ebc6a7fcbed06858fca62607
Denis Podvalenchuk <108662274+DenisPodvalenchuk@users.noreply.github.com>
2023-03-04 11:24:40 +0700

    2 коммит
    Изменение внешней оболочки. Проверка загрузки.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-
 import streamlit as st
 import pickle
 
-st.write('Hello, world!')
+st.title('Heart diseases prediction - Прогнозирование сердечных заболеваний')
+st.write('Изменение шкал признаков для получения прогноза')
 
 def load ():
     with open("c:/project-new/model.pcl", "rb") as fid:
@@ -13,6 +13,24 @@ height = st.slider('Рост', 143, 186, key='height ')
 weight = st.slider('Вес', 40, 107, key='weight ')
 ap_hi = st.slider('Давление верх', 93, 169, key='ap_hi ')
 ap_lo = st.slider('Давление низ', 66, 104, key='ap_lo ')
+
+st.sidebar.subheader(' Признаки: ')
+st.sidebar.write('- id — индекс пациента')
+st.sidebar.write('- age — возраст пациента')
+st.sidebar.write('- gender — пол пациента')
+st.sidebar.write('- height — рост пациента, см')
+st.sidebar.write('- weight — вес пациента, кг')
+st.sidebar.write('- ap_hi — систолическое давление(верхнее)')
+st.sidebar.write('- ap_lo — диастолическое давление(нижнее)')
+st.sidebar.write('- cholesterol — уровень холестерина')
+st.sidebar.write('- gluc — уровень глюкозы')
+st.sidebar.write('- smoke — курящий пациент')
+st.sidebar.write('- alco — отношение к алкоголю у пациента')
+st.sidebar.write('- active — физическая активность пациента')
+
+st.sidebar.subheader(' Целевой признак ')
+st.sidebar.write('- Cardio — risk of heart diseases')
+
 
 model = load()
 
